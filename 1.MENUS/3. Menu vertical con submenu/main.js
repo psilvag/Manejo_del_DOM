fd47fb5c6cc82084f7652cc6ef4f2527c55f2875
Pablo Silva <pmarcelosilvag@gmail.com>
2023-07-30 19:10:17 -0400

    fix: main.js

--- 1.MENUS/3. Menu vertical con submenu/main.js
+++ 1.MENUS/3. Menu vertical con submenu/main.js
@@ -2,16 +2,15 @@
 		
 			var botones_array = document.getElementsByClassName("boton");
 			for (var i = 0; i < botones_array.length; i++) {
-				botones_array[i].addEventListener("click", function(){
-					var submenu = this.nextElementSibling;
-					this.classList.toggle("activo");
-					
-					if (submenu.style.display==="block") {
+				
+                botones_array[i].addEventListener("click", function(){
+					let submenu = this.nextElementSibling;
+				    if (submenu.style.display==="block") {
 						submenu.style.display = "none";
 					} else {
 						submenu.style.display = "block";
 					}
-				} ,false)
+				})
 			}
 		
 	
